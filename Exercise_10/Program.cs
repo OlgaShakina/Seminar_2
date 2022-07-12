902b9db2d3ef2a6a9276c72bad0625456fecb03e
Olga <olga.buinova@gmail.com>
2022-07-13 00:03:19 +0300

    Добавили упражнения

---
+++ Exercise_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+int n=Convert.ToInt32(Console.ReadLine());
+if (n%100==0){
+    Console.WriteLine("Число меньше 100");
+}
+else{Console.WriteLine(Convert.ToString(n)[2]);
+}
---
+++ Exercise_13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.WriteLine("Введите число");
+int n=Convert.ToInt32(Console.ReadLine());
+if (n<100){
+    Console.WriteLine("Третьей цифры нет");
+}
+else{Console.WriteLine(Convert.ToString(n)[2]);
+}
